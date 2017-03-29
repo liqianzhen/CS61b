eaacd2af3399237f3128a967c516258ee387fd68
Qianzhen Li <qianzhenli@Qianzhens-MacBook-Pro.local>
2017-03-28 23:27:53 -0500

    updated linkedlistDeque

--- proj1/LinkedListDeque.java
+++ proj1/LinkedListDeque.java
@@ -10,9 +10,51 @@ public class LinkedListDeque<Blorp> {
         size = 0;
     }
 
-    public void addFirst(Blorp item) {
-        sentinel= new Node(null,null,null);
-        sentinel.next=new Node(sentinel,item,sentinel);
+    public LinkedListDeque(Blorp a) {
+        sentinel = new Node(null, null, null);
+        sentinel.next= new Node(sentinel,a,sentinel);
         sentinel.previous=sentinel.next;
+        size = 1;
+    }
+
+    public void addFirst(Blorp item) {
+        sentinel.next=new Node(sentinel,item,sentinel.next);
+        sentinel.next.next.previous=sentinel.next;
+        size +=1;
+    }
+
+    public void addback(Blorp item) {
+        sentinel.previous=new Node(sentinel.previous,item,sentinel);
+        sentinel.previous.previous.next=sentinel.previous;
+        size +=1;
+    }
+
+    public boolean isEmpty() {
+        if (sentinel==null) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node p= sentinel;
+        while (p.next!=sentinel) {
+            p=p.next;
+            System.out.print(p.item);
+        }
+    }
+
+    public Item removeFirst() {
+        sentinel.next=sentinel.next.next;
+        sentinel.next.previous=sentinel;
+    }
+
+    public Item removeLast() {
+        sentinel.previous=sentinel.previous.previous;
+        sentinel.previous.next=sentinel;
     }
 }
