62cd87788abaa1e30c4741ff5120f948cbea20d9
unknown <>
2017-04-04 00:28:36 -0500

    updated ArrayDeque.java

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -1,5 +1,24 @@
 /**
  * Created by qianzhenli on 3/31/17.
  */
-public class ArrayDeque {
+public class ArrayDeque<blorp> {
+    public blorp [] items;
+    public int size;
+    public int nextFirst;
+    public int nextLast;
+
+    public ArrayDeque() {
+        size=0;
+        items=(blorp[]) new Object [8];
+        nextFirst=0;
+        nextLast=0;
+    }
+
+    public void addFirst(blorp x) {
+        items[nextFirst+size]=x;
+        nextLast=
+    }
+
+
+
 }
