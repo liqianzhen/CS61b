f2976713b6188f27df6508e1185be72a2485977e
Qianzhen Li <qianzhenli@Qianzhens-MacBook-Pro.local>
2017-04-05 16:21:35 -0500

    update

--- Discussion3/Slist.java
+++ Discussion3/Slist.java
@@ -1,24 +1,46 @@
 public class Slist {
     private class Snode {
-        private int items;
+        private int item;
         private Snode next;
 
         private Snode (int item, Snode next) {
-            items = item;
-            next = next;
+            this.item = item;
+            this.next = next;
         }
-
     }
+
     private Snode front;
     public int size;
+
+    public Slist() {
+        front=null;
+        size=0;
+    }
 
     public void insertfront(int x) {
         front=new Snode(x,front);
         size+=1;
     }
-/*
-    public void insert(int item, int position) {
 
+    public void insert(int item, int position) {
+        Snode previous=GetLocation(position);
+        previous=new Snode(item,previous.next);
+    }
+
+    private Snode GetLocation(int position) {
+        if (position>size-1) {
+            position=size-1;
+        }
+        Snode p=front;
+        while (position!=0) {
+            p=p.next;
+            position-=1;
+        }
+        return p;
+    }
+
+    public void reverse () {
+        
     }
 
 }
