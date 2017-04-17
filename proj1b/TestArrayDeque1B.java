796e80f6ea89d3ad5e959d0b12cd9fbaf3c53ae2
Qianzhen Li <qianzhenli@Qianzhens-MacBook-Pro.local>
2017-04-16 23:45:53 -0500

    complete 30%

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -1,38 +1,43 @@
-/**
- * Created by chien on 4/14/2017.
- */
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestArrayDeque1B {
+
     @Test
     public void TestStudentArrayDeque() {
         StudentArrayDeque<Integer> student = new StudentArrayDeque<Integer>();
         ArrayDequeSolution<Integer> solution= new ArrayDequeSolution<Integer>();
 
-        int i=StdRandom.uniform(3);
-        int MethodCounter=0;
+        int expected=0;
+        int answer=0;
+        int size=0;
 
-        if (i==0) {
-            solution.addFirst(8);
-            student.addFirst(8);
-            assertEquals(solution.get(0),student.get(0));
-            MethodCounter++;
-        } else if (i==1 && MethodCounter>0) {
-             student.removeFirst();
-             solution.removeFirst();
-             MethodCounter--;
-        } else if (i==2) {
-             student.addLast(3);
-             solution.addLast(3);
-             ass
-             MethodCounter++;
-        } else if (i==3 && MethodCounter>0) {
-             student.removeLast();
-             solution.removeLast();
-             MethodCounter--;
+        for(int j=0;j<1000;j++) {
+
+            int i=StdRandom.uniform(5);
+
+            if (i==0) {
+                int a=StdRandom.uniform(9);
+                solution.addFirst(a);
+                student.addFirst(a);
+                size++;
+            } else if (i==1 && size>0) {
+                expected=student.removeFirst();
+                answer=solution.removeFirst();
+                size--;
+                assertEquals(expected,answer);
+            } else if (i==2) {
+                int b=StdRandom.uniform(9);
+                student.addLast(b);
+                solution.addLast(b);
+                size++;
+            } else if (i==3 && size>0) {
+                expected=student.removeLast();
+                answer=solution.removeLast();
+                assertEquals(expected,answer);
+                size--;
+            }
+
         }
-
     }
 }
