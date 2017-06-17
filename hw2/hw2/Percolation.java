d301c9327a031c92712d66d848d2db6db11f3dce
Qianzhen Li <qianzhenli@Qianzhens-MacBook-Pro.local>
2017-06-17 13:43:30 -0500

    finish hw 2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,72 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean [] [] sites;
+    private WeightedQuickUnionUF connection;
+    private int openSize;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        sites = new boolean[N][N];
+        connection = new WeightedQuickUnionUF(N * N + 2);
+    }
+
+    public void open(int row, int col) {
+        if (row > sites.length-1 || col > sites.length-1 ) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            sites[row][col] = true;
+            openSize += 1;
+            connectOpenSites(row, col);
+        }
+        if (row == 0) {
+            connection.union(sites.length * sites.length +1, xyTo1D(row, col));
+        } else if (row == sites.length -1 ) {
+            connection.union(sites.length * sites.length, xyTo1D(row, col));
+        }
+    }
+
+    private void connectOpenSites(int row, int col) {
+        if (row > 0 && isOpen(row - 1, col)) {
+            connection.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+        }
+        if (row < sites.length -1 && isOpen(row + 1, col)) {
+            connection.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+        }
+        if (col > 0 && isOpen(row, col - 1)) {
+            connection.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+        }
+        if (col < sites.length-1 && isOpen(row, col + 1)) {
+            connection.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+        }
+    }
+
+
+    public boolean isOpen(int row, int col) {
+        if (row > sites.length-1 || col > sites.length-1 ) {
+            throw new IndexOutOfBoundsException();
+        }
+        return sites[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        return connection.connected(sites.length * sites.length +1, xyTo1D(row, col));
+    }
+
+    public int numberOfOpenSites() {
+        return openSize;
+    }
+
+    public boolean percolates() {
+        return connection.connected(sites.length * sites.length +1, sites.length * sites.length);
+    }
+
+    private int xyTo1D(int r, int c) {
+        return r * sites.length + c;
+    }
+
+    public static void main(String[] args) {}
 
 }                       
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,52 @@
-package hw2;                       
+package hw2;
+
+import edu.princeton.cs.algs4.StdRandom;
 
 public class PercolationStats {
+    private Percolation Monte;
+    private int [] times;
+    private int size;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        times = new int[T];
+        size = N;
+
+        for (int j= 0; j < T; j++) {
+            Monte = new Percolation(N);
+
+            while (!Monte.percolates()) {
+                times[j] += 1;
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
+                Monte.open(row, col);
+            }
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        int total = 0;
+        for (int i: times) {
+            total += i;
+        }
+        return total/times.length;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        double std = 0;
+        for (int i : times) {
+            std +=(i - mean())*(i - mean());
+        }
+        return std/(times.length - 1);
+    }
+
+    // low  endpoint of 95% confidence interval
+    //public double confidenceLow()
+
+    // high endpoint of 95% confidence interval
+   //public double confidenceHigh()
+
 
 }                       
