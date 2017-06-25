f37d604f98bc544b5789dddc0b7dc1b647f4e5ed
Qianzhen Li <qianzhenli@Qianzhens-MacBook-Pro.local>
2017-06-25 15:32:12 -0500

    complete lab11

--- lab11/MazeBreadthFirstPaths.java
+++ lab11/MazeBreadthFirstPaths.java
@@ -1,4 +1,6 @@
-import java.util.Observable;
+import java.util.*;
+import java.util.ArrayDeque;
+
 /**
  *  @author Josh Hug
  */
@@ -9,20 +11,50 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at vertex x. */
     private void bfs(int s) {
-        /* Your code here. */
+        Queue<Integer> fringe = new ArrayDeque<>();
+        marked[s] = true;
+        announce();
+        fringe.add(s);
+
+        while (fringe.size() != 0 && !targetFound) {
+            int x = fringe.remove();
+            for (int i : maze.adj(x)) {
+                if (!marked[i]) {
+                    marked[i] = true;
+                    distTo[i] = distTo[x] + 1;
+                    edgeTo[i] = x;
+                    announce();
+                    fringe.add(i);
+                    if (i == t) {
+                        targetFound = true;
+                        break;
+                    }
+                }
+            }
+        }
     }
+
 
 
     @Override
     public void solve() {
-        // bfs(s);
+         bfs(s);
     }
 }
 
